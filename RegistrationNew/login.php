622724714535553e0507a85fd4836ba0df8661ed
Dazzy17 <65618412+Dazzy17@users.noreply.github.com>
2020-08-20 22:21:00 +0530

    made a new registration fully on php and css

--- RegistrationNew/login.php
+++ RegistrationNew/login.php
@@ -7,8 +7,15 @@
 
 <body class="body">
 
+    
+    
 
-    <form name="formlogin" class="formlogin">
+
+
+
+
+
+    <form name="formlogin" class="formlogin" method="POST" action="succesful.php">
 
         <h1>Login</h1>
 
@@ -17,31 +24,31 @@
         <label>User Name</label>
         <br>
 
-        <input type="text" class="textbox" name="uname" placeholder="username">
+        <input type="text" class="textbox" name="uname" placeholder="username" required>
         <br>
         <br>
         <label>Password</label>
         <br>
 
 
-        <input type="Password" class="textbox" name="pwd" placeholder="password">
+        <input type="Password" class="textbox" name="pwd" placeholder="password"required>
         <br>
         <table>
             <tr>
-                <td><button class="button1" type="Submit">Login</button></td>
+                <td><button class="button1">Login</button></td>
                 <td><button class="button2" type="reset">Clear</button></td>
             </tr>
         </table>
         <br>
 
-        <a href="hotelregvs.html" class="link">Not a Registered user?</a>
+        <a href="hotelreg.php" class="link">Not a Registered user?</a>
 
 
     </form>
 
 
 
-
+         
 
 
 
